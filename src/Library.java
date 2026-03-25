328efa966b97441c0d27055441a9ec1eff9ce7b1
Hans <hans79915@gmail.com>
2026-03-25 18:07:44 -0500

    added printReportHeader and event methods in library and started the switch case in Main. Also since we're kinda going for the government style approach of a library, I also added in a Archive class, idk if we'll need it but it seems fitting.

--- src/Library.java
+++ src/Library.java
@@ -4,9 +4,35 @@ class Library {
     int currentDay; // Current simulation day
 
     // add more fields here
-
-
     // add constructor(s) here
+
+    public void printReportHeader() {
+        System.out.println("""
+                OFFICIAL
+                END OF DAY DIRECTORATE REPORT.
+                
+                Department of Records and Archives
+                ACCESS: CLASSIFIED
+                """);
+    }
+
+    // Just a normal day
+    public void report0() {
+        printReportHeader();
+        // Something here eventually lol
+    }
+
+    public void report1() {
+
+    }
+
+    public void report2() {
+
+    }
+
+    public void report3() {
+
+    }
 
 
 }
